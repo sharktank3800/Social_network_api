8203f77645c0caf957e06e2ff3f6cccd55c433b5
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-04 22:26:24 -0400

    added .env file and imported packages files, set up env variables, created an instance of an express app, middleware tp parse incoming data established root route

---
+++ server.js
@@ -0,0 +1,16 @@
+// import packages and files
+const express = require("express");
+const routes = require("./routes");
+
+// setting up environment variables
+const PORT = process.env.PORT || 3333
+
+const app = express();
+
+// middleware to parse incoming data
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// use routes established in routes folder
+app.use(routes);
+
