76630276276941e88eae725ac76a82d3f22c8cbe
[Ayman Shiham] <sharktank38000@gmail.com>
2023-11-05 16:31:31 -0500

    finished models

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const {Schema, Types} = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: TimeStamp => new Date(TimeStamp).toLocaleDateString()
+        }
+    },
+
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+module.exports = reactionSchema;
